4dd327163f3d100c6043642b87198fd7c6772776
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-24 10:50:14 +0100

    Solve day 1 part 1

---
+++ src/day01/index.js
@@ -0,0 +1,25 @@
+import run from "aocrunner"
+
+const parseInput = rawInput => rawInput.split('\n').map(x => +x)
+
+const part1 = (rawInput) => {
+  const input = parseInput(rawInput)
+  return input.reduce((acc, x) => acc + x)
+
+  return
+}
+
+const part2 = (rawInput) => {
+  const input = parseInput(rawInput)
+
+  return
+}
+
+run({
+  part1: {
+    solution: part1,
+  },
+  part2: {
+    solution: part2,
+  },
+})
