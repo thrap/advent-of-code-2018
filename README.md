5ab7565fb87e9ece7c54dc759e6b9f58539f7d6f
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-24 11:21:15 +0100

    Solve day 2 part 2

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 <!--SOLUTIONS-->
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
-[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%86/yellow)](src/day02)
+[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
@@ -76,9 +76,9 @@ Both parts: 11.253195000000002ms
 
 ```
 Day 02
-Time part 1: 7.794ms
-Time part 2: -
-Both parts: 7.79391ms
+Time part 1: 6.693ms
+Time part 2: 12.476ms
+Both parts: 19.1691ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 3/50
-Total time: 19.047ms
+Total stars: 4/50
+Total time: 30.422ms
 ```
 
 <!--/RESULTS-->
--- src/day02/index.js
+++ src/day02/index.js
@@ -1,14 +1,10 @@
 import run from "aocrunner"
 
-const re = /(.*)/
-const parseLine = l => l.match(re).slice(1).map(x => +x ? +x : x)
-const parseInput = rawInput => rawInput.split('\n')//.map(parseLine)
-
-const part1 = (rawInput) => {
-  const input = parseInput(rawInput)
+const part1 = (input) => {
+  const ids = input.split('\n')
 
   const checksum = {}
-  input.forEach(([...x]) => {
+  ids.forEach(([...x]) => {
     const count = {}
     x.forEach(c => {
       count[c] = (count[c]||0) + 1
@@ -22,10 +18,32 @@ const part1 = (rawInput) => {
   return checksum[2]*checksum[3]
 }
 
-const part2 = (rawInput) => {
-  const input = parseInput(rawInput)
+const part2 = (input) => {
+  const ids = input.split('\n')
 
-  return
+  var min = Number.MAX_VALUE
+  var correct = []
+  for (var i = 0; i < ids.length; i++) {
+    const a = ids[i]
+    for (var j = i + 1; j < ids.length; j++) {
+      const b = ids[j]
+      var diff = 0
+      for (var c = 0; c < a.length; c++) {
+        if (a[c] != b[c])
+          diff++
+      }
+      if (diff <= min) {
+        min = diff
+        correct = [a, b]
+      }
+    }
+  }
+  const [a, b] = correct
+  var str = ''
+  for (var c = 0; c < a.length; c++) {
+    str += a[c] != b[c] ? '' : a[c]
+  }
+  return str
 }
 
 run({
