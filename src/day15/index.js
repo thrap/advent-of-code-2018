dd660ae24aeaa7107b65440410f4c8700c7f7495
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-28 15:15:37 +0200

    Refactor day 15

--- src/day15/index.js
+++ src/day15/index.js
@@ -38,19 +38,17 @@ const aStar = (grid, start, goal, maxSize=64) => {
       return moves
     }
 
-    const pos = dirs.filter(([dx, dy]) => (grid[x + dx]?.[y+dy] == '.'))
-    pos.forEach(([dx, dy]) => {
+    dirs.forEach(([dx, dy]) => {
+      var key = grid.length*(x+dx) + y+dy
+      if(!(grid[x + dx]?.[y+dy] == '.') || seen[key] <= moves + 1) return
+
       var man = Math.abs(goalX - (x+dx)) + Math.abs(goalY - (y+dy))
 
-      var key = grid.length*(x+dx) + y+dy
-      if (seen[key] <= moves + 1)
-        return
       seen[key] = moves + 1
 
       states.push([x+dx, y+dy, moves + 1])
       queue.push(states.length-1, moves + 1 + man)
     })
-
   }
   return Infinity
 }
@@ -70,18 +68,7 @@ const toStr = (grid, units=[], diff={}) => {
   ).join('') + unitStr(i)).join('\n')
 }
 
-var printed = {}
-
-const print = (grid, units, diff) => {
-  const str = toStr(grid, units, diff)
-  if(printed[str]) return
-  printed[str] = true
-  console.log(str)
-  console.log(units);
-  console.log()
-}
-
-const simulate = (map, units, prints, part2) => {
+const simulate = (map, units, part2) => {
   const move = unit => {
     const gridStr = toStr(map, units).split('\n')
     const grid = gridStr.map(l => l.split(''))
@@ -91,40 +78,32 @@ const simulate = (map, units, prints, part2) => {
     const isEnemy = ([i, j]) => {
       return grid[i][j] == (unit.type == 'G' ? 'E' : 'G')
     }
-    const targets = units.filter(t => t.type != unit.type)
-    const unitPos = [unit.i, unit.j]
 
     const around = (unit) => dirs.map(([di, dj]) => [unit.i+di, unit.j+dj])
-    const emptyAround = (unit) => around(unit).filter(isEmpty)
-
     if (around(unit).some(isEnemy)) {
       return
     }
 
+    const targets = units.filter(t => t.type != unit.type)
+    const unitPos = [unit.i, unit.j]
+    const emptyAround = (unit) => around(unit).filter(isEmpty)
+
     const range = targets.map(emptyAround).flat().sort((a, b) => manhattan(a, unitPos)-manhattan(b, unitPos))
 
     var minLength = Number.MAX_VALUE
-    const allCandidates = range.map((pos) => {
-      /** OPTIMALISERINGER:
-       * SJEKK AT IKKE STUCK
-       * Sorter gutta først?
-       * */
+    const candidates = range.map((pos) => {
       if (minLength < manhattan(pos, unitPos))
         return [0, Infinity]
       const distance = aStar(gridStr, unitPos, pos)
       minLength = Math.min(minLength, distance)
       return [pos, distance]
-    }).filter(x => x[1] == minLength)
-    const candidates = allCandidates.map(x => x[0])
+    }).filter(x => x[1] == minLength).map(x => x[0])
 
     const chosen = candidates.sort(sort)[0]
     if (!chosen) {
       return
     }
     const newPos = emptyAround(unit).filter(pos => aStar(gridStr, pos, chosen) == minLength-1).sort(sort)[0]
-    if (!newPos) {
-      return
-    }
 
     unit.i = newPos[0]
     unit.j = newPos[1]
@@ -145,7 +124,6 @@ const simulate = (map, units, prints, part2) => {
   }
 
   const act = (unit) => {
-    units = units.filter(u => u.hp > 0)
     if (units.every(unit => unit.type == units[0].type)) return false
     if(unit.hp <= 0) {
       return true
@@ -155,26 +133,14 @@ const simulate = (map, units, prints, part2) => {
     return true
   }
 
-  for (var turns = 0; turns < 1900; turns++) {
+  for (var turns = 0; true; turns++) {
     const sorted = units.sort((a, b) => sort([a.i, a.j], [b.i, b.j]))
-
-    if (prints)
-      console.log("Turn", turns) | print(map, units)
 
     const acted = sorted.every(act)
     if (units.every(unit => unit.type == units[0].type)) {
-      if (prints) {
-        console.log(acted);
-        console.log(turns);
-        print(map, units)
-        console.log();
-        console.log((turns) * units.reduce((acc, u) => acc+u.hp, 0));
-        console.log((turns+1) * units.reduce((acc, u) => acc+u.hp, 0));
-      }
       return ((acted ? 1: 0) + turns) * units.reduce((acc, u) => acc+u.hp, 0)
     }
   }
-  return
 }
 
 const part1 = (input) => {
@@ -192,7 +158,7 @@ const part2 = (input) => {
   while (high > low) {
     var mid = Math.ceil((high + low)/2)
     try {
-      const res = simulate(map, units.map(u => u.type == 'E' ? {...u, attack:mid} : ({...u})), false, true)
+      const res = simulate(map, units.map(u => u.type == 'E' ? {...u, attack:mid} : ({...u})), true)
       results[mid] = res
       high = mid-1
     } catch (e) {
@@ -205,88 +171,9 @@ const part2 = (input) => {
 
 run({
   part1: {
-    tests: [
-{ input: `#######
-#G..#E#
-#E#E.E#
-#G.##.#
-#...#E#
-#...E.#
-#######`, expected: 36334 },
-{ input: `#######
-#E..EG#
-#.#G.E#
-#E.##E#
-#G..#.#
-#..E#.#
-#######`, expected: 39514 },
-{ input: `#######
-#.E...#
-#.#..G#
-#.###.#
-#E#G#G#
-#...#G#
-#######`, expected: 28944 },
-{ input: `#########
-#G......#
-#.E.#...#
-#..##..G#
-#...##..#
-#...#...#
-#.G...G.#
-#.....G.#
-######### `, expected: 18740 },
-{ input: `#######
-#.G...#
-#...EG#
-#.#.#G#
-#..G#E#
-#.....#
-#######`, expected: 27730 },
-    ],
     solution: part1,
   },
   part2: {
-    tests: [
-      { input: `#######
-#.G...#
-#...EG#
-#.#.#G#
-#..G#E#
-#.....#
-#######`, expected: 4988},
-      { input: `#######
-#E..EG#
-#.#G.E#
-#E.##E#
-#G..#.#
-#..E#.#
-#######`, expected: 31284},
-      { input: `#######
-#E.G#.#
-#.#G..#
-#G.#.G#
-#G..#.#
-#...E.#
-#######`, expected: 3478},
-      { input: `#######
-#.E...#
-#.#..G#
-#.###.#
-#E#G#G#
-#...#G#
-#######`, expected: 6474},
-      { input: `#########
-#G......#
-#.E.#...#
-#..##..G#
-#...##..#
-#...#...#
-#.G...G.#
-#.....G.#
-#########`, expected: 1140},
-    ],
     solution: part2,
   },
-  onlyTests: false,
 })
