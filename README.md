3c8970f75b9c7acf565de99dd60d6bd55bc22e16
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-04-07 21:14:16 +0200

    Solve day 24 part 2

--- README.md
+++ README.md
@@ -34,7 +34,7 @@
 [![Day](https://badgen.net/badge/21/%E2%98%85%E2%98%85/green)](src/day21)
 [![Day](https://badgen.net/badge/22/%E2%98%85%E2%98%85/green)](src/day22)
 [![Day](https://badgen.net/badge/23/%E2%98%85%E2%98%85/green)](src/day23)
-[![Day](https://badgen.net/badge/24/%E2%98%85%E2%98%86/yellow)](src/day24)
+[![Day](https://badgen.net/badge/24/%E2%98%85%E2%98%85/green)](src/day24)
 ![Day](https://badgen.net/badge/25/%E2%98%86%E2%98%86/gray)
 
 <!--/SOLUTIONS-->
@@ -230,9 +230,9 @@ Both parts: 1345.780986ms
 
 ```
 Day 24
-Time part 1: 89.931ms
-Time part 2: -
-Both parts: 89.930739ms
+Time part 1: 18.499ms
+Time part 2: 13.811ms
+Both parts: 32.310015ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 47/50
-Total time: 11159.92ms
+Total stars: 48/50
+Total time: 11102.299ms
 ```
 
 <!--/RESULTS-->
--- src/day24/index.js
+++ src/day24/index.js
@@ -23,10 +23,10 @@ const fight = ([immune, infection]) => {
   const sortVal = group => group.attack * group.units * 100 + group.initiative
   const sort = (a, b) => sortVal(b) - sortVal(a)
 
-  for (var i = 0; i < 1000; i++) {
+  const seen = {}
+  for (var i = 0; true; i++) {
     const targeted = new Set()
     const target = (group, defenders) => {
-      delete group['target']
       const valid = defenders.filter(x => !x.immune.includes(group.type) && !targeted.has(x.id) && x.units > 0)
       const weak = valid.filter(x => x.weak.includes(group.type))
       if (weak.length == 0) {
@@ -43,7 +43,14 @@ const fight = ([immune, infection]) => {
     immune.forEach(imm => target(imm, infection))
     infection.forEach(inf => target(inf, immune))
 
-    const all = immune.concat(infection).sort((a, b) => b.initiative - a.initiative)
+    const all = immune.concat(infection).filter(g => g.units > 0).sort((a, b) => b.initiative - a.initiative)
+
+    if (i % 1000 == 0) {
+      const str = all.map(g => g.units)
+      if (seen[str])
+        break
+      seen[str] = true
+    }
 
     var acted = false
     all.forEach(group => {
@@ -56,39 +63,32 @@ const fight = ([immune, infection]) => {
     if (!acted)
       break
   }
-  return [immune, infection]
+  return [immune.filter(g => g.units > 0), infection.filter(g => g.units > 0)]
 }
 
 const part1 = (rawInput) => {
   const [, infection] = fight(parseInput(rawInput))
 
-  return infection.filter(g => g.units > 0).reduce((acc, g) => acc + g.units, 0)
+  return infection.reduce((acc, g) => acc + g.units, 0)
 }
 
 const part2 = (rawInput) => {
-
+  for (var i = 1; i <= 1000; i++) {
+    const input = parseInput(rawInput)
+    input[0].forEach(group =>
+      group.attack += i
+    )
+    const [immune, infection] = fight(input)
+    if (immune.length > 0 && infection.length == 0)
+      return immune.reduce((acc, g) => acc + g.units, 0)
+  }
 }
 
-const part1Input = `Immune System:
-17 units each with 5390 hit points (weak to radiation, bludgeoning) with an attack that does 4507 fire damage at initiative 2
-989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3
-
-Infection:
-801 units each with 4706 hit points (weak to radiation) with an attack that does 116 bludgeoning damage at initiative 1
-4485 units each with 2961 hit points (immune to radiation; weak to fire, cold) with an attack that does 12 slashing damage at initiative 4`
-const part2Input = part1Input
 run({
   part1: {
-    tests: [
-      { input: part1Input, expected: 5216 },
-    ],
     solution: part1,
   },
   part2: {
-    tests: [
-      { input: part2Input, expected: '' },
-    ],
     solution: part2,
   },
-  onlyTests: false,
 })
